ad9228f7ee3b1b3afbbed35c3d29ce261dfd4776
damiog <damiog@gmail.com>
2009-05-05 19:23:20 +0000

    Applied a modified version of moonwatcher patch.

--- SublerCLI/SublerCLI.mm
+++ SublerCLI/SublerCLI.mm
@@ -8,7 +8,9 @@ void print_help()
     printf("\t\t-s set subtitle input file\n");
     printf("\t\t-d set delay in ms\n");
     printf("\t\t-a set height in pixel\n");
-    printf("\t\t-l set track language (ex. English)\n");
+    printf("\t\t-l set track language (i.e. English)\n");
+    printf("\t\t-n set track name\n");
+    printf("\t\t-r remove existing subtitles\n");
     printf("\t\t-h print this help information\n");
     printf("\t\t-v print version\n");
 }
@@ -22,9 +24,12 @@ int main (int argc, const char * argv[]) {
 
     char* input_file = NULL;
     char* input_sub = NULL;
+    char* name = "Subtitle Track";
     char* language = "English";
     int delay = 0;
     unsigned int height = 60;
+    BOOL removeExisting = false;
+    BOOL modified = false;
 
     if (argc == 1) {
         print_help();
@@ -32,7 +37,7 @@ int main (int argc, const char * argv[]) {
     }
 
     char opt_char=0;
-    while ((opt_char = getopt(argc, (char * const*)argv, "i:s:d:a:l:vh")) != -1) {
+    while ((opt_char = getopt(argc, (char * const*)argv, "i:s:d:a:l:n:rvh")) != -1) {
         switch(opt_char) {
             case 'h':
                 print_help();
@@ -57,6 +62,12 @@ int main (int argc, const char * argv[]) {
             case 'l':
                 language = optarg;
                 break ;
+            case 'n':
+                name = optarg;
+                break ;
+            case 'r':
+                removeExisting = true;
+                break ;                
             default:
                 print_help();
                 exit(-1);
@@ -64,7 +75,7 @@ int main (int argc, const char * argv[]) {
         }
     }
 
-    if (input_file && input_sub)
+    if (input_file && (input_sub || removeExisting))
     {
         NSError *outError;
         MP42File *mp4File;
@@ -74,15 +85,28 @@ int main (int argc, const char * argv[]) {
             printf("Error: %s\n", "the mp4 file couln't be open.");
             return -1;
         }
+        if (removeExisting) {
+            MP42Track *track;
+            for (track in mp4File.tracks)
+                if ([track isMemberOfClass:[MP42SubtitleTrack class]])
+                    if (track.muxed) {
+                        [mp4File removeTrackAtIndex:[mp4File.tracks indexOfObject:track]];
+                        modified = true;
+                    }
+        }
 
-        [mp4File addTrack:[MP42SubtitleTrack subtitleTrackFromFile:[NSString stringWithCString:input_sub
+        if (input_sub) {
+            MP42SubtitleTrack *track = [MP42SubtitleTrack subtitleTrackFromFile:[NSString stringWithCString:input_sub
                                                                                                    encoding:NSUTF8StringEncoding]
                                                                           delay:delay
                                                                          height:height
                                                                        language:[NSString stringWithCString:language
-                                                                                                   encoding:NSUTF8StringEncoding]]];
+                                                                                                   encoding:NSUTF8StringEncoding]];
+            track.name = [NSString stringWithCString:name encoding:NSUTF8StringEncoding];
+            [mp4File addTrack:track];
+        }
 
-        if (![mp4File updateMP4File:&outError]) {
+        if (modified && ![mp4File updateMP4File:&outError]) {
             printf("Error: %s\n", [[outError localizedDescription] UTF8String]);
             return -1;
         }
