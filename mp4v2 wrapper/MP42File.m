c23fd82504a6f82ec1afc353a4f2f1dc026cdf17
damiog <damiog@gmail.com>
2010-12-05 10:27:28 +0000

    Check if previewImages count is not zero. Fix a crash.

--- mp4v2 wrapper/MP42File.m
+++ mp4v2 wrapper/MP42File.m
@@ -384,7 +384,7 @@ NSString * const MP42CreateChaptersPreviewTrack = @"ChaptersPreview";
                             waitUntilDone:YES];
 
         // If we haven't got enought images, return.
-        if ([previewImages count] < [[chapterTrack chapters] count]) {
+        if (([previewImages count] < [[chapterTrack chapters] count]) || [previewImages count] == 0 ) {
             [pool release];
             return NO;
         }
