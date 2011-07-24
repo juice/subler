60d6a1123d1a63b313eaa90bf3839198b742add1
damiog <damiog@gmail.com>
2011-07-24 10:49:10 +0000

    Plain Text subs from mkv needs to be converted.

--- mp4v2 wrapper/MP42Utilities.m
+++ mp4v2 wrapper/MP42Utilities.m
@@ -557,7 +557,7 @@ int readAC3Config(uint64_t acmod, uint64_t lfeon, UInt32 *channelsCount, UInt32 
 
 BOOL isTrackMuxable(NSString * formatName)
 {
-    NSArray* supportedFormats = [NSArray arrayWithObjects:@"H.264", @"MPEG-4 Visual", @"AAC", @"AC-3", @"3GPP Text", @"Text", @"Plain Text",
+    NSArray* supportedFormats = [NSArray arrayWithObjects:@"H.264", @"MPEG-4 Visual", @"AAC", @"AC-3", @"3GPP Text", @"Text",
                                  @"CEA-608", /*@"Photo - JPEG",*/ @"VobSub", nil];
 
     for (NSString* type in supportedFormats)
@@ -568,7 +568,7 @@ BOOL isTrackMuxable(NSString * formatName)
 }
 
 BOOL trackNeedConversion(NSString * formatName) {
-    NSArray* supportedConversionFormats = [NSArray arrayWithObjects:@"Vorbis", @"DTS", @"Flac", @"Mp3", @"True HD", @"ASS", @"SSA", nil];
+    NSArray* supportedConversionFormats = [NSArray arrayWithObjects:@"Vorbis", @"DTS", @"Flac", @"Mp3", @"True HD", @"ASS", @"SSA", @"Plain Text", nil];
 
     for (NSString* type in supportedConversionFormats)
         if ([formatName isEqualToString:type])
