181e8a96d335fa30db820b64bbe68bf54d3497b1
damiog <damiog@gmail.com>
2011-08-12 16:32:50 +0000

    Remove some unneeded lines of code.

--- mp4v2 wrapper/SBVobSubConverter.m
+++ mp4v2 wrapper/SBVobSubConverter.m
@@ -205,48 +205,6 @@ static ComponentResult ReadPacketControls(UInt8 *packet, UInt32 palette[16], Pac
 	return noErr;
 }
 
-//	==============================================================
-//	resizedImage
-//	==============================================================
-// Return a scaled copy of the image.  
-
-
-CGImageRef createResizedImage(CGImageRef imageRef, CGRect thumbRect)
-{
-	CGImageAlphaInfo	alphaInfo = CGImageGetAlphaInfo(imageRef);
-	
-	// There's a wierdness with kCGImageAlphaNone and CGBitmapContextCreate
-	// see Supported Pixel Formats in the Quartz 2D Programming Guide
-	// Creating a Bitmap Graphics Context section
-	// only RGB 8 bit images with alpha of kCGImageAlphaNoneSkipFirst, kCGImageAlphaNoneSkipLast, kCGImageAlphaPremultipliedFirst,
-	// and kCGImageAlphaPremultipliedLast, with a few other oddball image kinds are supported
-	// The images on input here are likely to be png or jpeg files
-	if (alphaInfo == kCGImageAlphaNone)
-		alphaInfo = kCGImageAlphaNoneSkipLast;
-    
-	// Build a bitmap context that's the size of the thumbRect
-	CGContextRef bitmap = CGBitmapContextCreate(
-                                                NULL,
-                                                thumbRect.size.width,		// width
-                                                thumbRect.size.height,		// height
-                                                CGImageGetBitsPerComponent(imageRef),	// really needs to always be 8
-                                                4 * thumbRect.size.width,	// rowbytes
-                                                CGImageGetColorSpace(imageRef),
-                                                alphaInfo
-                                                );
-    
-	// Draw into the context, this scales the image
-	CGContextDrawImage(bitmap, thumbRect, imageRef);
-    
-	// Get an image from the context and a UIImage
-	CGImageRef	ref = CGBitmapContextCreateImage(bitmap);
-    
-	CGContextRelease(bitmap);	// ok if NULL
-	//CGImageRelease(ref);
-    
-	return ref;
-}
-
 @implementation SBVobSubConverter
 
 - (void) DecoderThreadMainRoutine: (id) sender
