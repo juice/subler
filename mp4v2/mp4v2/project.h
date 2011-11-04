9c417889e496fd1bc7242e7dc13e1e89f17777a5
damiog <damiog@gmail.com>
2011-11-04 10:06:47 +0000

    Update libmp4v2 to rev 487

--- mp4v2/mp4v2/project.h
+++ mp4v2/mp4v2/project.h
@@ -6,13 +6,13 @@
 #define MP4V2_PROJECT_name            "MP4v2"
 #define MP4V2_PROJECT_name_lower      "mp4v2"
 #define MP4V2_PROJECT_name_upper      "MP4V2"
-#define MP4V2_PROJECT_name_formal     "MP4v2 trunk-r454"
+#define MP4V2_PROJECT_name_formal     "MP4v2 trunk-r487"
 #define MP4V2_PROJECT_url_website     "http://code.google.com/p/mp4v2"
 #define MP4V2_PROJECT_url_downloads   "http://code.google.com/p/mp4v2/downloads/list"
 #define MP4V2_PROJECT_url_discussion  "http://groups.google.com/group/mp4v2"
 #define MP4V2_PROJECT_irc             "irc://irc.freenode.net/handbrake"
 #define MP4V2_PROJECT_bugreport       "<eddyg@myreflection.org>"
-#define MP4V2_PROJECT_version         "trunk-r454"
+#define MP4V2_PROJECT_version         "trunk-r487"
 #define MP4V2_PROJECT_version_hex     0x00020000
 #define MP4V2_PROJECT_version_major   2
 #define MP4V2_PROJECT_version_minor   0
@@ -21,10 +21,10 @@
 #define MP4V2_PROJECT_repo_branch     "trunk"
 #define MP4V2_PROJECT_repo_root       "http://mp4v2.googlecode.com/svn"
 #define MP4V2_PROJECT_repo_uuid       "6e6572fa-98a6-11dd-ad9f-f77439c74b79"
-#define MP4V2_PROJECT_repo_rev        454
-#define MP4V2_PROJECT_repo_date       "2011-02-04 18:55:55 +0100 (Fri, 04 Feb 2011)"
+#define MP4V2_PROJECT_repo_rev        487
+#define MP4V2_PROJECT_repo_date       "2011-10-19 20:03:14 +0200 (Wed, 19 Oct 2011)"
 #define MP4V2_PROJECT_repo_type       "developer"
-#define MP4V2_PROJECT_build           "Sun Feb  6 10:22:16 CET 2011"
+#define MP4V2_PROJECT_build           "Fri Nov  4 10:44:47 CET 2011"
 
 /*****************************************************************************/
 
